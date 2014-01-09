5bef91f8a7f38665105f1ed55e47d5d246e7b51c
yuleskaperez <yuleskaperez@gmail.com>
2014-01-09 14:54:30 +0000

    Hice a benzema

--- ISBCTournament/src/Team/Manager.java
+++ ISBCTournament/src/Team/Manager.java
@@ -18,11 +18,13 @@ public class Manager extends TeamManager{
 			return getBehaviour(0);
 		else if (id == 2 || id == 4)
 			return getBehaviour(2);
+		else if (id==3)
+			return getBehaviour(3);
 		return getBehaviour(1);
 	}
 	
 	public Behaviour[] createBehaviours() {
-		return new Behaviour[] {new GoalKeeper(),new GoToBall(),new Lateral() };
+		return new Behaviour[] {new GoalKeeper(),new GoToBall(),new Lateral(),new Forward() };
 	}
 	
 }
