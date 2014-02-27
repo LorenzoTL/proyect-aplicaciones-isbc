df0c66863a0dbdcb80d16e9f9949cc146221568c
jge1591 <jge1591@gmail.com>
2014-02-27 16:31:55 +0000

    Upgraded forward

--- ISBCTournament/src/Team/F.java
+++ ISBCTournament/src/Team/F.java
@@ -1,5 +1,6 @@
 package Team;
 
+import teams.ucmTeam.RobotAPI;
 import EDU.gatech.cc.is.util.Vec2;
 
 public class F {
@@ -15,6 +16,21 @@ public class F {
 	
 	public static boolean estoyCerca(Vec2 origen,Vec2 destino){
 		return origen.distance(destino) <= 0.72;
+	}
+	
+	public static boolean estaDetrasBall(RobotAPI r) {
+        double ang = r.getBall().t;
+        // Transformamos el ángulo a positivo
+        while (ang < 0)
+                ang += Math.PI*2;
+        // Accuracy
+        double accuracy = Math.PI/9;
+        if ((r.getFieldSide() == -1 && (ang < accuracy || ang > 2*Math.PI-accuracy)))// ||
+                return true;
+        if ((r.getFieldSide() == 1 && (ang > Math.PI-accuracy && ang < Math.PI-accuracy)))
+                return true;
+        else
+                return false;
 	}
 	
 }
