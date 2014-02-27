db45484d153c90ba9a66a055c5a4efbe20c64cf0
jge1591@gmail.com <jge1591@gmail.com@08e66afc-cb18-7fd6-39bc-7c57260b32c2>
2014-02-27 17:29:37 +0000

    Changes to goalKeeper

--- trunk/ISBCTournament/src/Team/GoalKeeper.java
+++ trunk/ISBCTournament/src/Team/GoalKeeper.java
@@ -20,25 +20,16 @@ public class GoalKeeper extends Behaviour{
 	}
 	
 	public int takeStep() {
-		if (lado == -1){
-			if (myRobotAPI.getBall().x < 0.35 && myRobotAPI.getBall().y < 0.4 && 
-				 myRobotAPI.getPosition().x < -1.15 && (myRobotAPI.getPosition().y < 0.35 && myRobotAPI.getPosition().y > -0.35)) {
+		if (myRobotAPI.blocked())
+            myRobotAPI.avoidCollisions();
+		
+			if (myRobotAPI.getBall().r < 0.5 && myRobotAPI.getOurGoal().r < 0.45){
 				this.setState(new Salida());
-			} else if (Math.abs(myRobotAPI.getOurGoal().x) > 0.1 || Math.abs(myRobotAPI.getOurGoal().y) > 0.01) {
+			} else if (myRobotAPI.getOurGoal().r >= 0.07){
 				this.setState(new IrPorteria());
 			} else {
 				this.setState(new Parado());
 			}
-		}else{
-			if (myRobotAPI.getBall().x > -0.35 && myRobotAPI.getBall().y < 0.4 && 
-					 myRobotAPI.getPosition().x > 1.15 && (myRobotAPI.getPosition().y < 0.35 && myRobotAPI.getPosition().y > -0.35)) {
-					this.setState(new Salida());
-				} else if (Math.abs(myRobotAPI.getOurGoal().x) > 0.1 || Math.abs(myRobotAPI.getOurGoal().y) > 0.01) {
-					this.setState(new IrPorteria());
-				} else {
-					this.setState(new Parado());
-				}
-		}
 		this.state.action();
 		return myRobotAPI.ROBOT_OK;
 	}
@@ -65,7 +56,7 @@ public class GoalKeeper extends Behaviour{
 		//Sub clases que implementan la interfaz
 		private class Salida implements KeeperState{
 			public void action(){
-				myRobotAPI.setSpeed(3.0);
+				myRobotAPI.setSpeed(1.5);
 				myRobotAPI.setSteerHeading(myRobotAPI.getBall().t);
 				myRobotAPI.surroundPoint(myRobotAPI.getPosition(), myRobotAPI.getBall());
 				if (myRobotAPI.canKick()) {
