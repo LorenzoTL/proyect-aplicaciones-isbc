4f22e837133e02b63e6080c7578e2397e82050df
hecmar091@gmail.com <hecmar091@gmail.com@08e66afc-cb18-7fd6-39bc-7c57260b32c2>
2014-04-21 23:03:59 +0000

    Reorganización de la arquitectura del proyecto.

--- trunk/Recomendador/src/Viviendas/ViviendasConnector.java
+++ trunk/Recomendador/src/Viviendas/ViviendasConnector.java
@@ -1,10 +1,13 @@
 package Viviendas;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 import jcolibri.cbrcore.CBRCase;
 import jcolibri.cbrcore.CaseBaseFilter;
@@ -28,6 +31,40 @@ public class ViviendasConnector implements Connector {
 	@Override
 	public void storeCases(Collection<CBRCase> cases) {
 		// TODO Auto-generated method stub
+		try{
+			ArrayList<String> lines = new ArrayList<String>();
+			BufferedReader reader = new BufferedReader(new FileReader("src/Viviendas/viviendas2"));
+			if (reader == null) return;
+			String line = null;
+			Iterator<CBRCase> it = cases.iterator();
+			int id = (it.hasNext()) ? ((DescripcionVivienda)it.next().getDescription()).getId() : -1; 
+			if (id == -1) return;
+			while ((line=reader.readLine())!=null){
+				String[] s = line.split("#");
+				if(Integer.valueOf(s[0]) == id){
+					int v = Integer.valueOf(s[19]) + 1;
+					s[19] = "" + v;
+					String cadena = s[0];
+					for(int i=1;i<s.length;i++)
+						cadena = cadena + "#" + s[i];
+					lines.add(cadena);
+				}else{
+					lines.add(line);
+				}
+			}
+			reader.close();
+			
+			BufferedWriter writer = new BufferedWriter(new FileWriter("src/Viviendas/viviendas2"));
+			if (writer == null) return;
+			for(Iterator<String> ite=lines.iterator();ite.hasNext();){
+				String s = ite.next();
+				writer.write(s);
+				writer.newLine();
+			}
+			writer.close();
+		}catch(Exception e){
+			 
+		}
 
 	}
 
@@ -42,7 +79,7 @@ public class ViviendasConnector implements Connector {
 		ArrayList<CBRCase> cases = new ArrayList<CBRCase>();
 		try
 		{
-			BufferedReader reader = new BufferedReader(new FileReader("viviendas"));
+			BufferedReader reader = new BufferedReader(new FileReader("src/Viviendas/viviendas"));
 			String line = null;
 			while ((line=reader.readLine())!=null)
 			{
@@ -70,9 +107,7 @@ public class ViviendasConnector implements Connector {
 	public Collection<CBRCase> retrieveSomeCases(CaseBaseFilter filter) {
 		// TODO Auto-generated method stub
 		return null;
-	}
-	
-	
+	}	
 	/**
 	 * Testing method
 	 * @param args
@@ -80,8 +115,10 @@ public class ViviendasConnector implements Connector {
 	public static void main(String[] args){
 		ViviendasConnector vc = new ViviendasConnector();
 		Collection<CBRCase> cases = vc.retrieveAllCases();
-		for(CBRCase c : cases)
+		for(CBRCase c : cases){
 			System.out.println(c);
+			break;
+		}
 	}
 
 }
