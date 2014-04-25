0b114086a80e4cea559ec72bd70b09d1723efb63
hecmar091 <hecmar091@gmail.com>
2014-04-25 13:53:43 +0000

    Comenzando la interfaz.

---
+++ Recomendador/src/Clases_Auxiliares/Preferencias.java
@@ -0,0 +1,35 @@
+package Clases_Auxiliares;
+
+public class Preferencias {
+		private String localizacion;
+		private int habitaciones;
+		private int superficie;
+		
+		public Preferencias(){
+			
+		}
+		
+		public int getHabitaciones() {
+			return habitaciones;
+		}
+		
+		public String getLocalizacion() {
+			return localizacion;
+		}
+		
+		public int getSuperficie() {
+			return superficie;
+		}
+		
+		public void setHabitaciones(int habitaciones) {
+			this.habitaciones = habitaciones;
+		}
+		
+		public void setLocalizacion(String localizacion) {
+			this.localizacion = localizacion;
+		}
+		
+		public void setSuperficie(int superficie) {
+			this.superficie = superficie;
+		}
+}
---
+++ Recomendador/src/Interfaz/FormularioInicial.java
@@ -0,0 +1,13 @@
+package Interfaz;
+
+import javax.swing.JFrame;
+
+import Clases_Auxiliares.Preferencias;
+
+public class FormularioInicial extends JFrame {
+
+		public ConfigPanel<Preferencias> creaPanelConfiguracion(){
+			//TODO
+		}
+	
+}
