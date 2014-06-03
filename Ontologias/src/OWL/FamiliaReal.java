ae041979d4117f167878a56bed1ad66a79ea9b0d
hecmar091 <hecmar091@gmail.com>
2014-06-03 16:55:47 +0000

    Interfaz y consultas.

--- Ontologias/src/OWL/FamiliaReal.java
+++ Ontologias/src/OWL/FamiliaReal.java
@@ -2,7 +2,6 @@ package OWL;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-
 import es.ucm.fdi.gaia.ontobridge.OntoBridge;
 import es.ucm.fdi.gaia.ontobridge.OntologyDocument;
 
@@ -89,10 +88,6 @@ public class FamiliaReal {
 		ob.createOntProperty("Sofia", "es_progenitor", "Elena");
 		ob.createOntProperty("Sofia", "es_progenitor", "Cristina");
 		
-		ob.createOntProperty("Felipe", "es_hermano", "Elena");
-		ob.createOntProperty("Felipe", "es_hermano", "Cristina");
-		ob.createOntProperty("Cristina", "es_hermano", "Elena");
-		
 		ob.createOntProperty("Felipe", "esta_casado_con", "Leticia");
 		ob.createOntProperty("Felipe", "es_progenitor", "Infanta_Leonor");
 		ob.createOntProperty("Felipe", "es_progenitor", "Infanta_Sofia");
@@ -114,6 +109,10 @@ public class FamiliaReal {
 		ob.createOntProperty("Elena", "es_progenitor", "Victoria");
 		ob.createOntProperty("Marichalar", "es_progenitor", "Froilan");
 		ob.createOntProperty("Marichalar", "es_progenitor", "Victoria");
+		
+		ob.createOntProperty("Felipe", "es_hermano", "Elena");
+		ob.createOntProperty("Felipe", "es_hermano", "Cristina");
+		ob.createOntProperty("Cristina", "es_hermano", "Elena");
 	}
 	
 	public void createInstanceFoto(){
@@ -129,7 +128,7 @@ public class FamiliaReal {
 		System.out.println("\n CREACION DE PROPIEDADES DE LAS FOTOS");
 		
 		/** Propiedades **/
-		ob.createDataTypeProperty("foto1", "url_Foto", "file:Fotos/foto1.png");
+		ob.createDataTypeProperty("foto1", "url_Foto", "file:Fotos/foto1.png#");
 		ob.createOntProperty("foto1", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto1", "aparece", "Sofia");
 		ob.createOntProperty("foto1", "aparece", "Felipe");
@@ -147,7 +146,7 @@ public class FamiliaReal {
 		ob.createOntProperty("foto1", "aparece", "Froilan");
 		ob.createOntProperty("foto1", "aparece", "Victoria");
 		
-		ob.createDataTypeProperty("foto2", "url_Foto", "file:Fotos/foto2.png");
+		ob.createDataTypeProperty("foto2", "url_Foto", "file:Fotos/foto2.png#");
 		ob.createOntProperty("foto2", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto2", "aparece", "Sofia");
 		ob.createOntProperty("foto2", "aparece", "Felipe");
@@ -165,7 +164,7 @@ public class FamiliaReal {
 		ob.createOntProperty("foto2", "aparece", "Froilan");
 		ob.createOntProperty("foto2", "aparece", "Victoria");
 		
-		ob.createDataTypeProperty("foto3", "url_Foto", "file:Fotos/foto3.jpg");
+		ob.createDataTypeProperty("foto3", "url_Foto", "file:Fotos/foto3.jpg#");
 		ob.createOntProperty("foto3", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto3", "aparece", "Sofia");
 		ob.createOntProperty("foto3", "aparece", "Felipe");
@@ -174,7 +173,7 @@ public class FamiliaReal {
 		ob.createOntProperty("foto3", "aparece", "Urdangarin");
 		ob.createOntProperty("foto3", "aparece", "Elena");
 		
-		ob.createDataTypeProperty("foto4", "url_Foto", "file:Fotos/foto4.jpg");
+		ob.createDataTypeProperty("foto4", "url_Foto", "file:Fotos/foto4.jpg#");
 		ob.createOntProperty("foto4", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto4", "aparece", "Sofia");
 		ob.createOntProperty("foto4", "aparece", "Felipe");
@@ -182,13 +181,13 @@ public class FamiliaReal {
 		ob.createOntProperty("foto4", "aparece", "Infanta_Leonor");
 		ob.createOntProperty("foto4", "aparece", "Infanta_Sofia");
 		
-		ob.createDataTypeProperty("foto5", "url_Foto", "file:Fotos/foto5.jpg");
+		ob.createDataTypeProperty("foto5", "url_Foto", "file:Fotos/foto5.jpg#");
 		ob.createOntProperty("foto5", "aparece", "Felipe");
 		ob.createOntProperty("foto5", "aparece", "Leticia");
 		ob.createOntProperty("foto5", "aparece", "Infanta_Leonor");
 		ob.createOntProperty("foto5", "aparece", "Infanta_Sofia");
 		
-		ob.createDataTypeProperty("foto6", "url_Foto", "file:Fotos/foto6.jpg");;
+		ob.createDataTypeProperty("foto6", "url_Foto", "file:Fotos/foto6.jpg#");;
 		ob.createOntProperty("foto6", "aparece", "Infanta_Leonor");
 		ob.createOntProperty("foto6", "aparece", "Infanta_Sofia");
 		ob.createOntProperty("foto6", "aparece", "Irene");
@@ -198,13 +197,13 @@ public class FamiliaReal {
 		ob.createOntProperty("foto6", "aparece", "Froilan");
 		ob.createOntProperty("foto6", "aparece", "Victoria");
 		
-		ob.createDataTypeProperty("foto7", "url_Foto", "file:Fotos/foto7.png");
+		ob.createDataTypeProperty("foto7", "url_Foto", "file:Fotos/foto7.png#");
 		ob.createOntProperty("foto7", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto7", "aparece", "Sofia");
 		ob.createOntProperty("foto7", "aparece", "Felipe");
 		ob.createOntProperty("foto7", "aparece", "Leticia");
 		
-		ob.createDataTypeProperty("foto8", "url_Foto", "file:Fotos/foto8.jpg");
+		ob.createDataTypeProperty("foto8", "url_Foto", "file:Fotos/foto8.jpg#");
 		ob.createOntProperty("foto8", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto8", "aparece", "Sofia");
 		ob.createOntProperty("foto8", "aparece", "Elena");
@@ -212,29 +211,29 @@ public class FamiliaReal {
 		ob.createOntProperty("foto8", "aparece", "Froilan");
 		ob.createOntProperty("foto8", "aparece", "Victoria");
 		
-		ob.createDataTypeProperty("foto9", "url_Foto", "file:Fotos/foto9.png");
+		ob.createDataTypeProperty("foto9", "url_Foto", "file:Fotos/foto9.png#");
 		ob.createOntProperty("foto9", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto9", "aparece", "Felipe");
 		
-		ob.createDataTypeProperty("foto10", "url_Foto", "file:Fotos/foto10.png");
+		ob.createDataTypeProperty("foto10", "url_Foto", "file:Fotos/foto10.png#");
 		ob.createOntProperty("foto10", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto10", "aparece", "Felipe");
 		
-		ob.createDataTypeProperty("foto11", "url_Foto", "file:Fotos/foto11.png");
+		ob.createDataTypeProperty("foto11", "url_Foto", "file:Fotos/foto11.png#");
 		ob.createOntProperty("foto11", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto11", "aparece", "Felipe");
 		
-		ob.createDataTypeProperty("foto12", "url_Foto", "file:Fotos/foto12.png");
+		ob.createDataTypeProperty("foto12", "url_Foto", "file:Fotos/foto12.png#");
 		ob.createOntProperty("foto12", "aparece", "Juan_Carlos");
 		
-		ob.createDataTypeProperty("foto13", "url_Foto", "file:Fotos/foto13.png");
+		ob.createDataTypeProperty("foto13", "url_Foto", "file:Fotos/foto13.png#");
 		ob.createOntProperty("foto13", "aparece", "Infanta_Leonor");
 		ob.createOntProperty("foto13", "aparece", "Infanta_Sofia");
 		
-		ob.createDataTypeProperty("foto14", "url_Foto", "file:Fotos/foto14.jpg");
+		ob.createDataTypeProperty("foto14", "url_Foto", "file:Fotos/foto14.jpg#");
 		ob.createOntProperty("foto14", "aparece", "Elena");
 		
-		ob.createDataTypeProperty("foto15", "url_Foto", "file:Fotos/foto15.jpg");
+		ob.createDataTypeProperty("foto15", "url_Foto", "file:Fotos/foto15.jpg#");
 		ob.createOntProperty("foto15", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto15", "aparece", "Sofia");
 		ob.createOntProperty("foto15", "aparece", "Felipe");
@@ -243,15 +242,15 @@ public class FamiliaReal {
 		ob.createOntProperty("foto15", "aparece", "Urdangarin");
 		ob.createOntProperty("foto15", "aparece", "Elena");
 		
-		ob.createDataTypeProperty("foto16", "url_Foto", "file:Fotos/foto16.jpg");
+		ob.createDataTypeProperty("foto16", "url_Foto", "file:Fotos/foto16.jpg#");
 		ob.createOntProperty("foto16", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto16", "aparece", "Sofia");
 		ob.createOntProperty("foto16", "aparece", "Felipe");
 		
-		ob.createDataTypeProperty("foto17", "url_Foto", "file:Fotos/foto17.jpg");
+		ob.createDataTypeProperty("foto17", "url_Foto", "file:Fotos/foto17.jpg#");
 		ob.createOntProperty("foto17", "aparece", "Juan_Carlos");
 		
-		ob.createDataTypeProperty("foto18", "url_Foto", "file:Fotos/foto18.jpg");
+		ob.createDataTypeProperty("foto18", "url_Foto", "file:Fotos/foto18.jpg#");
 		ob.createOntProperty("foto18", "aparece", "Cristina");
 		ob.createOntProperty("foto18", "aparece", "Urdangarin");
 		ob.createOntProperty("foto18", "aparece", "Irene");
@@ -259,7 +258,7 @@ public class FamiliaReal {
 		ob.createOntProperty("foto18", "aparece", "Pablo");
 		ob.createOntProperty("foto18", "aparece", "Juan");
 		
-		ob.createDataTypeProperty("foto19", "url_Foto", "file:Fotos/foto19.JPG");
+		ob.createDataTypeProperty("foto19", "url_Foto", "file:Fotos/foto19.JPG#");
 		ob.createOntProperty("foto19", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto19", "aparece", "Sofia");
 		ob.createOntProperty("foto19", "aparece", "Felipe");
@@ -277,32 +276,41 @@ public class FamiliaReal {
 		ob.createOntProperty("foto19", "aparece", "Froilan");
 		ob.createOntProperty("foto19", "aparece", "Victoria");
 		
-		ob.createDataTypeProperty("foto20", "url_Foto", "file:Fotos/foto20.jpg");
+		ob.createDataTypeProperty("foto20", "url_Foto", "file:Fotos/foto20.jpg#");
 		ob.createOntProperty("foto20", "aparece", "Felipe");
 		ob.createOntProperty("foto20", "aparece", "Leticia");
 		
-		ob.createDataTypeProperty("foto21", "url_Foto", "file:Fotos/foto21.jpg");
+		ob.createDataTypeProperty("foto21", "url_Foto", "file:Fotos/foto21.jpg#");
 		ob.createOntProperty("foto21", "aparece", "Juan_Carlos");
 		
-		ob.createDataTypeProperty("foto22", "url_Foto", "file:Fotos/foto22.jpg");
+		ob.createDataTypeProperty("foto22", "url_Foto", "file:Fotos/foto22.jpg#");
 		ob.createOntProperty("foto22", "aparece", "Juan_Carlos");
 		
-		ob.createDataTypeProperty("foto23", "url_Foto", "file:Fotos/foto23.jpg");
+		ob.createDataTypeProperty("foto23", "url_Foto", "file:Fotos/foto23.jpg#");
 		ob.createOntProperty("foto23", "aparece", "Juan_Carlos");
 		ob.createOntProperty("foto23", "aparece", "Sofia");
 		ob.createOntProperty("foto23", "aparece", "Felipe");
 		ob.createOntProperty("foto23", "aparece", "Leticia");
 	}
 	
-	public void instanceInfered(String s){
+	public ArrayList<String> instanceInfered(String s){
 		System.out.println("\n INSTANCIAS INFERIDAS PARA " + s.toUpperCase());
-		
+		ArrayList<String> list = new ArrayList<String>();
 		Iterator<String> fotos = ob.listInstances(s);
 		while(fotos.hasNext())
 		{
 			String instance = fotos.next();
-			System.out.println(instance);
+			ArrayList<String> properties = new ArrayList<String>();
+			ArrayList<String> values = new ArrayList<String>();
+			ob.listInstancePropertiesValues(instance, properties, values);
+			for(int i=0; i<properties.size(); i++){
+				if(properties.get(i).contains("url_Foto")){
+					String[] value = values.get(i).split("#");
+					list.add(value[0]);
+				}
+			}
 		}
+		return list;
 	}
 	
 }
