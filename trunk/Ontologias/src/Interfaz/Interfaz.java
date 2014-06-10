897f0cea4fe953eb63ff70553ec19fb144a8d5d8
jge1591@gmail.com <jge1591@gmail.com@08e66afc-cb18-7fd6-39bc-7c57260b32c2>
2014-06-10 10:57:44 +0000

    Creacion panel tabulado

--- trunk/Ontologias/src/Interfaz/Interfaz.java
+++ trunk/Ontologias/src/Interfaz/Interfaz.java
@@ -14,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.border.EmptyBorder;
 
 import OWL.FamiliaReal;
@@ -23,6 +24,7 @@ public class Interfaz extends JFrame{
 	private ArrayList<String> results;
 	
 	private JPanel contentPane;
+	private JPanel contentPane2;
 	private JPanel panel;
 	
 	private JButton buttonAssert;
@@ -30,6 +32,18 @@ public class Interfaz extends JFrame{
 	private JLabel label;
 	private JComboBox<String> comboBox;
 	private JScrollPane scrollPane;
+	
+	private JPanel panelFoto;
+	private JButton buttonAssert2;
+	private JButton buttonNietos;
+	private JButton buttonSobrinos;
+	private JButton buttonRey;
+	private JButton buttonFamiliares;
+	private JButton buttonDeportes;
+	private JButton buttonTrabajo;
+	private JScrollPane scrollPaneCentral;
+	
+	private JLabel[] labelFotos;
 	
 	private JCheckBox checkBox;
 	private JCheckBox checkBox_1;
@@ -60,9 +74,11 @@ public class Interfaz extends JFrame{
 		
 		results = new ArrayList<String>();
 		
+		JTabbedPane tabbedPane = new JTabbedPane();
+		setContentPane(tabbedPane);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		String[] paths = {"---","Fotos/foto1.png","Fotos/foto2.png","Fotos/foto3.jpg","Fotos/foto4.jpg"
@@ -86,10 +102,10 @@ public class Interfaz extends JFrame{
 	                scrollPane.remove(panel);
 	                panel = null;
 	                creaPanelCentral(s);
+					buttonAssert2.setEnabled(true);
 				}
 				revalidate();
                 repaint();
-                buttonAssert.setEnabled(true);
 			}
 		});
 		contentPane.add(btnCargarFoto);
@@ -171,49 +187,220 @@ public class Interfaz extends JFrame{
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         contentPane.add(scrollPane);
 		
+		buttonAssert2 = new JButton("ASSERT");
+		buttonAssert2.setBounds(265, 535, 97, 23);
+		buttonAssert2.setEnabled(false);
+        buttonAssert2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+						if (comboBox != null && comboBox.getSelectedItem()!= null && !
+										comboBox.getSelectedItem().toString().equals("---")){
+								String foto = getFotoSelected();
+								ArrayList<String> personas = getPeopleSelected();
+								fr.assertPropertieAparece(foto, personas);
+						}
+				}
+		});
+		contentPane.add(buttonAssert2);
+		tabbedPane.addTab("Panel Usuario", contentPane);
+		// Fin panel usuario
+		
+		contentPane2 = new JPanel();
+		contentPane2.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane2.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 157, 437);
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 1;
 		buttonAssert = new JButton("ASSERT");
-		buttonAssert.setBounds(265, 535, 97, 23);
-		buttonAssert.setEnabled(false);
 		buttonAssert.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (comboBox != null && comboBox.getSelectedItem()!= null && !
-						comboBox.getSelectedItem().toString().equals("---")){
-					String foto = getFotoSelected();
-					ArrayList<String> personas = getPersonsSelected();
-					fr.assertPropertieAparece(foto, personas);
-				}
+				fr.createPropertiesFotos();
+				fr.createPropertiesFotosObjetos();
+				fr.createPropertiesFamilia();
+				buttonNietos.setEnabled(true);
+				buttonSobrinos.setEnabled(true);
+				buttonRey.setEnabled(true);
+				buttonFamiliares.setEnabled(true);
+				buttonDeportes.setEnabled(true);
+				buttonTrabajo.setEnabled(true);
+				buttonAssert.setEnabled(false);
 			}
 		});
-		contentPane.add(buttonAssert);
+		panel.add(buttonAssert,c);
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weightx = 1;
+		buttonNietos = new JButton("Nietos del rey");
+		buttonNietos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (scrollPaneCentral != null) {
+					contentPane2.remove(scrollPaneCentral);
+					scrollPaneCentral.remove(panelFoto);
+					panelFoto = null;
+				}
+				//Método al que pasar la lista de URL de las fotos
+				results = fr.instanceInfered("Fotos_Nietos_del_Rey");
+				creaPanelCentralFotos(results);
+				revalidate();
+				repaint();
+			}
+		});
+		buttonNietos.setEnabled(false);
+		panel.add(buttonNietos,c);
+		
+		c.gridx = 0;
+		c.gridy = 2;
+		c.weightx = 1;
+		buttonSobrinos = new JButton("Sobrinos del principe");
+		buttonSobrinos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (scrollPaneCentral != null) {
+					contentPane2.remove(scrollPaneCentral);
+					scrollPaneCentral.remove(panelFoto);
+					panelFoto = null;
+				}
+				results = fr.instanceInfered("Fotos_Sobrinos_del_principe");
+				creaPanelCentralFotos(results);
+				revalidate();
+				repaint();
+			}
+		});
+		buttonSobrinos.setEnabled(false);
+		panel.add(buttonSobrinos,c);
+		
+		c.gridx = 0;
+		c.gridy = 3;
+		c.weightx = 1;
+		buttonRey = new JButton("El Rey");
+		buttonRey.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (scrollPaneCentral != null) {
+					contentPane2.remove(scrollPaneCentral);
+					scrollPaneCentral.remove(panelFoto);
+					panelFoto = null;
+				}
+				results = fr.instanceInfered("Fotos_Rey");
+				creaPanelCentralFotos(results);
+				revalidate();
+				repaint();
+			}
+		});
+		buttonRey.setEnabled(false);
+		panel.add(buttonRey,c);
+		
+		c.gridx = 0;
+		c.gridy = 4;
+		c.weightx = 1;
+		buttonFamiliares = new JButton("Fotos Familiares");
+		buttonFamiliares.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (scrollPaneCentral != null) {
+					contentPane2.remove(scrollPaneCentral);
+					scrollPaneCentral.remove(panelFoto);
+					panelFoto = null;
+				}
+				results = fr.instanceInfered("Fotos_familiares");
+				creaPanelCentralFotos(results);
+				revalidate();
+				repaint();
+			}
+		});
+		buttonFamiliares.setEnabled(false);
+		panel.add(buttonFamiliares,c);
+		
+		c.gridx = 0;
+		c.gridy = 5;
+		c.weightx = 1;
+		buttonDeportes = new JButton("Eventos deportivos");
+		buttonDeportes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (scrollPaneCentral != null) {
+					contentPane2.remove(scrollPaneCentral);
+					scrollPaneCentral.remove(panelFoto);
+					panelFoto = null;
+				}
+				results = fr.instanceInfered("Fotos_eventos_deportivos");
+				creaPanelCentralFotos(results);
+				revalidate();
+				repaint();
+			}
+		});
+		buttonDeportes.setEnabled(false);
+		panel.add(buttonDeportes,c);
+		
+		c.gridx = 0;
+		c.gridy = 6;
+		c.weightx = 1;
+		buttonTrabajo = new JButton("Fotos de trabajo");
+		buttonTrabajo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (scrollPaneCentral != null) {
+					contentPane2.remove(scrollPaneCentral);
+					scrollPaneCentral.remove(panelFoto);
+					panelFoto = null;
+				}
+				results = fr.instanceInfered("Fotos_de_trabajo");
+				creaPanelCentralFotos(results);
+				revalidate();
+				repaint();
+			}
+		});
+		buttonTrabajo.setEnabled(false);
+		panel.add(buttonTrabajo,c);
+		
+		JScrollPane scrollPane = new JScrollPane(panel);
+		scrollPane.setBounds(10, 11, 157, 437);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		contentPane2.add(scrollPane);
+		
+		panelFoto = new JPanel();
+		panelFoto.setBounds(177, 11, 437, 437);
+		panelFoto.setLayout(new GridBagLayout());
+		
+		scrollPaneCentral = new JScrollPane(panelFoto);
+		scrollPaneCentral.setBounds(177, 11, 437, 437);
+		scrollPaneCentral.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPaneCentral.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		contentPane2.add(scrollPaneCentral);
+		
+		tabbedPane.addTab("Panel busquedas", contentPane2);
 	}
 	
-	private ArrayList<String> getPersonsSelected(){
-		ArrayList<String> list = new ArrayList<String>();
-		if (checkBox.isSelected()) list.add("Juan_Carlos");
-		if (checkBox_1.isSelected()) list.add("Cristina");
-		if (checkBox_2.isSelected()) list.add("Sofia");
-		if (checkBox_3.isSelected()) list.add("Urdangarin");
-		if (checkBox_4.isSelected()) list.add("Elena");
-		if (checkBox_5.isSelected()) list.add("Felipe");
-		if (checkBox_6.isSelected()) list.add("Irene");
-		if (checkBox_7.isSelected()) list.add("Marichalar");
-		if (checkBox_8.isSelected()) list.add("Leticia");
-		if (checkBox_9.isSelected()) list.add("Miguel");
-		if (checkBox_10.isSelected()) list.add("Froilan");
-		if (checkBox_11.isSelected()) list.add("Infanta_Leonor");
-		if (checkBox_12.isSelected()) list.add("Pablo");
-		if (checkBox_13.isSelected()) list.add("Victoria");
-		if (checkBox_14.isSelected()) list.add("Infanta_Sofia");
-		if (checkBox_15.isSelected()) list.add("Juan");
-		return list;
-	}
+	private ArrayList<String> getPeopleSelected() {
+                ArrayList<String> list = new ArrayList<String>();
+                if (checkBox.isSelected()) list.add("Juan_Carlos");
+                if (checkBox_1.isSelected()) list.add("Cristina");
+                if (checkBox_2.isSelected()) list.add("Sofia");
+                if (checkBox_3.isSelected()) list.add("Urdangarin");
+                if (checkBox_4.isSelected()) list.add("Elena");
+                if (checkBox_5.isSelected()) list.add("Felipe");
+                if (checkBox_6.isSelected()) list.add("Irene");
+                if (checkBox_7.isSelected()) list.add("Marichalar");
+                if (checkBox_8.isSelected()) list.add("Leticia");
+                if (checkBox_9.isSelected()) list.add("Miguel");
+                if (checkBox_10.isSelected()) list.add("Froilan");
+                if (checkBox_11.isSelected()) list.add("Infanta_Leonor");
+                if (checkBox_12.isSelected()) list.add("Pablo");
+                if (checkBox_13.isSelected()) list.add("Victoria");
+                if (checkBox_14.isSelected()) list.add("Infanta_Sofia");
+                if (checkBox_15.isSelected()) list.add("Juan");
+                return list;
+    }
 	
-	private String getFotoSelected(){
-		String foto = comboBox.getSelectedItem().toString();
-		String[] f = foto.split("/");
-		foto = f[1].split(".png")[0];
-		return foto;
-	}
+	private String getFotoSelected() {
+                String foto = comboBox.getSelectedItem().toString();
+                String[] f = foto.split("/");
+                foto = f[1].split(".png")[0];
+                return foto;
+    }
 	
 	public void creaPanelCentral(String s) {
 		ImageIcon icon = new ImageIcon(s);
@@ -233,13 +420,61 @@ public class Interfaz extends JFrame{
         scrollPane.setBounds(65, 42, 466, 325);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        getContentPane().add(scrollPane);
+        contentPane.add(scrollPane);
+	}
+	
+	public void creaPanelCentralFotos(ArrayList<String> listFotos) {
+		labelFotos = new JLabel[listFotos.size()];
+		
+		panelFoto = new JPanel();
+		panelFoto.setBounds(177, 11, 437, 437);
+		panelFoto.setLayout(new GridBagLayout());
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		int i = 0;
+		int j = 0;
+		
+		c.gridx = 0;
+		c.gridy = i + j;
+		c.weightx = 1;
+		JLabel label = new JLabel(" ");
+		
+		panelFoto.add(label,c);
+		j++;
+		
+		for (String foto : listFotos) {
+			
+			String[] path = foto.split("file:");
+			ImageIcon icon = new ImageIcon(path[1]); 
+			
+			c.gridx = 0;
+			c.gridy = i + j;
+			c.weightx = 1;
+			labelFotos[i] = new JLabel(icon);
+			
+			panelFoto.add(labelFotos[i],c);
+			i++;
+			
+			c.gridx = 0;
+			c.gridy = i + j;
+			c.weightx = 1;
+			label = new JLabel(" ");
+			
+			panelFoto.add(label,c);
+			j++;
+		}
+		scrollPaneCentral = new JScrollPane(panelFoto);
+		scrollPaneCentral.setBounds(177, 11, 437, 437);
+		scrollPaneCentral.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPaneCentral.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		contentPane2.add(scrollPaneCentral);
 	}
 	
 	public static void main(String[] args){
 		Interfaz interfaz = new Interfaz();
-		interfaz.setSize(640,610);
-		interfaz.setLocation(350, 125);
+		interfaz.setSize(640,635);
+		interfaz.setLocation(350, 100);
 		interfaz.setVisible(true);
 	}
 }
